30af488b4dbc8bcbaa2a61e09640b346c89aa80b
safrinauriya <safrina_auriya_28rpl@student.smktelkom-mlg.sch.id>
2020-02-17 11:43:47 +0700

    fisrt commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RESTORAN</title>
+    <link rel="stylesheet" href="utama.css" type="text/css">
+<style>
+.button {
+    background-color: crimson;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 12px;
+    width: 605px;
+    float: center;
+}
+.button:hover{
+    background-color: violet;
+}
+</style>
+</head>
+<body>
+   <div class="container">
+        <header>
+            <h1 class="judul" align="center">>>RESTORAN MAMASUKA<<</h1>
+        </header>
+   </div>
+   <div>
+        <h1>
+        SELAMAT DATANG
+        <br>
+        SILAHKAN PILIH DIBAWAH INI
+        </h1>
+   </div>
+   <a href="pembeli.php" class="button">PEMBELI</a>
+   <a href="admin.php" class="button">ADMIN</a>
+</body>
+</html>
